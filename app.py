737cdd5a70fd4174e0e81e78813421ad558e4d8b
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-16 12:31:06 +0000

    notes on flask, creating some pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -17,6 +17,29 @@ def home():
 @app.route('/api/v1/student/data', methods = ['GET'])
 def customised_api():
     return jsonify(students) # transforms data into Json 
+
+# simple page to greet user
+@app.route('/welcome/')
+def greet_user():
+    return 'Welcome to the DevOps team.'
+
+
+
+## find a user the module to redirect the user back to the welcome page
+## if page is not found (404) redirect the user to a welcome page
+# @app.route('/')
+# def page_not_found():
+#     return redirect("/Welcome/", code=404)
+
+# redirecting to a page
+@app.route('/login')
+def login():
+    return redirect('/welcome/')
+
+# taking input, returning fstring
+@app.route('/<username>/')
+def welcome_user(username):
+    return f'<h1>Welcome to the dream team of DevOps {username}</h1>'
 
 if __name__ == 'main':
     app.run(debug=True)
